c82eb4c799f3487d7cab8393e298acd53e9137b1
Eric <guanhongmingz@outlook.com>
2025-10-27 19:03:25 +0800

    enable local dev

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,12 +15,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,192.168.0.64',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    '%s%s',
+    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+    env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+))),
 
     /*
     |--------------------------------------------------------------------------
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,4 +4,4 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://192.168.0.64';
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
